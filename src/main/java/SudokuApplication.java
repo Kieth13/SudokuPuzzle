4621519b800cfa7aac4c9153666f9d821b97625d
Kieth13 <ana.sartiukova@gmail.com>
2022-08-08 00:42:58 +0300

    Initial commit

---
+++ src/main/java/SudokuApplication.java
@@ -0,0 +1,28 @@
+import UI.IUserInterfaceContract;
+import UI.UserInterfaceImpl;
+import buildLogic.SudokuBuildLogic;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SudokuApplication extends Application {
+    private IUserInterfaceContract.View uiImpl;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        //Get domain.SudokuGame object for a new game
+        uiImpl = new UserInterfaceImpl(primaryStage);
+
+        try {
+            SudokuBuildLogic.build(uiImpl);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
